b826429638b64240dc4f5aa9aecde596904ceae6
ju570r1u5 <albertodeprezzo@gmail.com>
2023-08-06 19:18:18 +0200

    basic refactoring on xor definition

--- enclib/one_time_pad.py
+++ enclib/one_time_pad.py
@@ -1,7 +1,7 @@
 import random
 
 
-def get_key_stream(size: int) -> bytes:
+def gen_key_stream(size: int) -> bytes:
     """generate a key stream of a given size
 
     Args:
--- enclib/test_one_time_pad.py
+++ enclib/test_one_time_pad.py
@@ -6,7 +6,7 @@ class TestOneTimePad(unittest.TestCase):
     def test_encrypt_decrypt(self) -> None:
         en_msg: bytes = "alberto ok".encode()
         lmsg: int = len(en_msg)
-        ks: bytes = one_time_pad.get_key_stream(lmsg)
+        ks: bytes = one_time_pad.gen_key_stream(lmsg)
 
         cipher: bytes = one_time_pad.encrypt(ks, en_msg)
         clean: bytes = one_time_pad.decrypt(cipher, ks)
