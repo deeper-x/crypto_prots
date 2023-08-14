215d9456c10e1057f625a7743b53db19c285a121
ju570r1u5 <albertodeprezzo@gmail.com>
2023-08-14 20:36:15 +0200

    generator - modulus arithmetic

--- enclib/test_generator.py
+++ enclib/test_generator.py
@@ -5,12 +5,16 @@ from enclib import generator
 
 
 class TestGenerator(unittest.TestCase):
-    def selfAssertGet(self) -> None:
-        # cg: Generator = Generator(7, 3)
-        # print(cg.get_sequence())
-
+    def test_get_valid(self) -> None:
         cg: generator.Sequence = generator.Sequence(7, 3)
         expected: list[int] = [1, 3, 2, 6, 4, 5]
+        got: Optional[list[int]] = cg.get()
+
+        self.assertEqual(expected, got)
+
+    def test_get_None(self) -> None:
+        cg: generator.Sequence = generator.Sequence(100, 2)
+        expected: None = None
         got: Optional[list[int]] = cg.get()
 
         self.assertEqual(expected, got)
