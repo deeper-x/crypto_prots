8e9fa9471c7973b5fa36e8a2c5dbcb88f2409cc7
ju570r1u5 <albertodeprezzo@gmail.com>
2023-08-18 12:53:47 +0200

    refactoring generator definition

---
+++ enclib/diffie_hellman.py
@@ -0,0 +1,49 @@
+from typing import Optional
+
+import random
+
+
+def is_prime(n_in: int) -> bool:
+    """Tells if a given number is prime
+
+    Args:
+        n_in (int): input number, integer
+
+    Returns:
+        bool: True if it's prime, False otherwise
+    """
+
+    if n_in == 1:
+        return False
+    elif n_in > 1:
+        for i in range(2, n_in):
+            if n_in % i == 0:
+                return False
+        return True
+    else:
+        return False
+
+
+def get_random_prime(in_random: int) -> Optional[int]:
+    """Recursive function for generating a random prime number in a given range
+
+    Args:
+        in_random (int): range for generating random number
+
+    Returns:
+        Optional[int]: prime number. None if not founded
+    """
+
+    cur: int = random.randrange(3, in_random)
+    if is_prime(cur):
+        return cur
+
+    get_random_prime(in_random)
+
+    return None
+
+
+def get_generator(in_prime: int) -> int:
+    res: int
+    
+    return res
--- enclib/generator.py
+++ enclib/generator.py
@@ -6,6 +6,8 @@ class Sequence():
         self.__p: int = p
         self.__g: int = g
         self.__res: list[int] = []
+        for i in range(self.__p-1):
+            self.__res.append(self.__g**i % self.__p)
 
     def get(self) -> Optional[list[int]]:
         """generate a sequence starting from g, modulus p
@@ -13,9 +15,6 @@ class Sequence():
         Returns:
             list[int]: generator sequence
         """
-
-        for i in range(self.__p-1):
-            self.__res.append(self.__g**i % self.__p)
 
         if self.__is_valid():
             return self.__res
@@ -30,13 +29,11 @@ class Sequence():
         """
 
         res: bool = False
-        check_set: set[int] = set()
+        check_set: set = set()
 
-        # each sequence's element is added to a set, if the element is between
-        # 0 and the maximum possible value of the list
-        # e.g: [1, 4, 5, 2, 3], or [1, 3, 2] or [1, 4, 5, 2, 3]
-        # So, in_seq is considered valid only if built on a valid succession of
-        # incremental numbers, with step 1
+        # use a set in order to check if it's a valid generator sequence
+        # input:    [1, 3, 2, 6, 4, 5]
+        # set:      (1, 3, 2, 6, 4, 5)
         for i in self.__res:
             if i > 0 and i <= len(self.__res):
                 check_set.add(i)
@@ -45,3 +42,8 @@ class Sequence():
             res = True
 
         return res
+
+
+if __name__ == "__main__":
+    c: Sequence = Sequence(7, 3)
+    print(c.get())
